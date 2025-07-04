bfc65f25ef1fad50dc61843bf555ca4980d71a94
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-04 14:17:34 +0300

    + ClientAdmin

--- clients/admin.py
+++ clients/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import Client
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'company', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('name', 'email', 'company')
