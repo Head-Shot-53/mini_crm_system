61a8c73b4544883e99bf53f6014643be14ab536c
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-04 16:50:30 +0300

    +accounts.urls

--- mini_crm/urls.py
+++ mini_crm/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clients.urls'))
+    path('', include('clients.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
