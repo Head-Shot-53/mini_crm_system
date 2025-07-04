833eeb2682b139440a94cb6d63d9d95196ed61a8
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-04 16:54:09 +0300

    +model Note

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from clients.models import Client
 
-# Create your models here.
+class Note(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='notes')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.client.name})'
+    
+    
