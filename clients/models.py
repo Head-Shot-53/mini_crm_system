d559213930a5aa5b840390c5103cc4d0579c617b
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-04 14:16:28 +0300

    +model Client

--- clients/models.py
+++ clients/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class Client(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новий'),
+        ('potential', 'Потенційний'),
+        ('active', 'Активний'),
+        ('inactive', 'Неактивний'),
+    ]
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20,blank=True)
+    company = models.CharField(max_length=255, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.status}'
