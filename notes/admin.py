590e08c3b4440e260d4f315150e0017072e8cb4d
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-04 16:57:52 +0300

    +NoteAdmin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'client', 'created_at')
+    list_filter = ('created_at', 'client')
+    search_fields = ('title', 'description')
