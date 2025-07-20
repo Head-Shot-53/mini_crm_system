bf9cd71d199294beb60fdc729dd5cde121c0f493
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-20 14:48:31 +0300

    + include notes.urls

--- mini_crm/urls.py
+++ mini_crm/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clients.urls')),
     path('accounts/', include('accounts.urls')),
+    path('notes/', include('notes.urls')),
 ]
