c770effbba61813354eb5c08566673a88e96f426
Head-Shot-53 <denchikmashtack@gmail.com>
2025-07-20 12:27:19 +0300

    + path for client_detail

--- clients/urls.py
+++ clients/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('add/', views.client_create, name='client_create'),
     path('<int:pk>/edit/', views.client_update, name='client_update'),
     path('<int:pk>/delete/', views.client_delete, name='client_delete'),
+    path('<int:pk>/', views.client_detail, name='client_detail'),
 ]
